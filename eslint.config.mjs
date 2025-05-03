ebfdc76c97366f33c47fbb6e8867e2089e4c124d
csbiy <katd6@naver.com>
2025-05-03 20:02:43 +0900

    fix : eslint type fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,7 @@ const eslintConfig = [
     rules: {
       'react/no-unescaped-entities': 'off',
       '@typescript-eslint/no-empty-object-type' : 'off',
+      '@typescript-eslint/no-explicit-any' :'off',
     },
   }),
 ];
